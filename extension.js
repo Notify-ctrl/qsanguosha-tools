504bc0d9365c33666eccfff6f42e9fbe0875e96d
Notify-ctrl <1969315774@qq.com>
2022-01-17 14:26:30 +0800

    fix inline completion

--- extension.js
+++ extension.js
@@ -1,10 +1,19 @@
 const vscode = require('vscode');
 const snippets = require('./snippets.json')
 
+// 获取这一行到目前为止的上一个不是字母的位置，没有的话返回0
+function getLastNonLetter(document, position) {
+    const line = document.getText(new vscode.Range(new vscode.Position(position.line, 0), position));
+    const result = line.search(/[^a-zA-Z][a-zA-Z]+$/);
+    return new vscode.Position(position.line, result === -1 ? 0 : result);
+}
+
 function activate(context) {
 	let completion = vscode.languages.registerCompletionItemProvider({language: 'lua', scheme: 'file'}, {
 		provideCompletionItems(document, position) {
-			const line = document.getText(new vscode.Range(new vscode.Position(position.line, 0), position));
+            let prevPosition = position;
+            prevPosition.character = Math.max(0, prevPosition.character - 1);
+			const line = document.getText(new vscode.Range(getLastNonLetter(document, prevPosition), position));
             const afterpos = new vscode.Range(position, new vscode.Position(position.line, position.character + 1));
             const after = document.getText(afterpos);
 
