f5b27d75237ea891568a93f882dd1ce9d15f1f14
Notify-ctrl <1969315774@qq.com>
2022-01-17 14:37:01 +0800

    现在可以处理管道符号（感谢梓喵）

--- extension.js
+++ extension.js
@@ -1,5 +1,16 @@
 const vscode = require('vscode');
-const snippets = require('./snippets.json')
+let snippets = require('./snippets.json')
+const card_snippets = require('./card.json')
+const engine_snippets = require('./engine.json')
+const player_snippets = require('./player.json')
+
+let loaded = false
+if (!loaded) {
+    snippets.push(...card_snippets);
+    snippets.push(...engine_snippets);
+    snippets.push(...player_snippets);
+    loaded = true;
+}
 
 // 获取这一行到目前为止的上一个不是字母的位置，没有的话返回0
 function getLastNonLetter(document, position) {
@@ -20,15 +31,21 @@ function activate(context) {
             let items = [];
 
             for (const snippet of snippets) {
-                if (snippet.token && line.match(new RegExp(`${snippet.token.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&")}(?![^\\w\\n\\s\\r\\-])[\\w\\n\\s\\r\\-]*`))) {
-                    let item = new vscode.CompletionItem(snippet.prefix);
-                    item.insertText = new vscode.SnippetString(snippet.body);
-                    item.documentation = new vscode.MarkdownString(snippet.desc);
-                    item.kind = snippet.kind;
-                    item.detail = snippet.detail || snippet.prefix;
-                    item.additionalTextEdits = (snippet.token.match(/^[\(\[\{]$/) && after.match(/[\}\]\)]/)) ? [vscode.TextEdit.delete(afterpos)] : null;
+                if (snippet.token) {
+                    for (t of snippet.token.split('|')) {
+                        if (line.match(new RegExp(`${t.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&")}(?![^\\w\\n\\s\\r\\-])[\\w\\n\\s\\r\\-]*`))) {
+                            let item = new vscode.CompletionItem(snippet.prefix);
+                            item.insertText = new vscode.SnippetString(snippet.body);
+                            item.documentation = new vscode.MarkdownString(snippet.desc);
+                            item.kind = snippet.kind;
+                            item.detail = snippet.detail || snippet.prefix;
+                            item.additionalTextEdits = (snippet.token.match(/^[\(\[\{]$/) && after.match(/[\}\]\)]/)) ? [vscode.TextEdit.delete(afterpos)] : null;
 
-                    items.push(item);
+                            items.push(item);
+
+                            break;
+                        }
+                    }
                 }
             }
 
